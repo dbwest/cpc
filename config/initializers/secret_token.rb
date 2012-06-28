e706240b39def7ccf5fd4803405eabfe978dbc52
David West <david.b.west@gmail.com>
2012-06-28 15:34:25 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cityparkconsulting::Application.config.secret_token = '0016cc24b703b6bafc8b23d80678ce154a2d614d5e682306385c74810164296a8ed68a22e1132af54c6ed08d6e2c4a36495c900043644fe4556c8d3f3032c6c8'
